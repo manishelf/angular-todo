50960294c2d64682944696e70462d24727899550
Manish patil <manishpatil7058@gmail.com>
2025-04-06 14:55:01 +0530

    test for oauth for google signin

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -14,7 +14,7 @@ import { CommonModule } from '@angular/common';
 export class NavbarComponent implements OnInit{
   accessToken:string | null = null;
   user: SocialUser = new SocialUser();
-  loggedIn: boolean = true;
+  loggedIn: boolean = false;
 
   constructor(private router: Router,  private authService: SocialAuthService) { }
 
