223a1c0b96f9c5d863dc74d7b06cf99840a981e4
Manish patil <manishpatil7058@gmail.com>
2025-07-07 22:08:13 +0530

    removed login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './page/login/login.component';
 import { HomeComponent } from './page/home/home.component';
 import { EditorComponent } from './page/editor/editor.component';
 import { CalendarComponent } from './page/calendar/calendar.component';
@@ -7,7 +6,6 @@ import { AboutComponent } from './page/about/about.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch: 'full'},
-    {path:'login', component: LoginComponent},
     {path:'create', component:EditorComponent},
     {path:'home', component: HomeComponent},
     {path: 'bin', component: HomeComponent},
