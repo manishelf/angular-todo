f358aab5ef05619c38c4e43385c9e132409cc497
Manish patil <manishpatil7058@gmail.com>
2025-07-08 21:06:20 +0530

    search clear bug fix

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -3,73 +3,87 @@ import { TodoItem } from './../../models/todo-item';
 import { TodoServiceService } from './../../service/todo-service.service';
 import { CommonModule } from '@angular/common';
 import { TodoItemComponent } from './../../component/todo-item/todo-item.component';
-import { ActivatedRoute, Params, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  ActivatedRoute,
+  Params,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   imports: [CommonModule, TodoItemComponent, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit, OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy {
   itemList: TodoItem[] = [];
   fromBin: boolean = false;
   fromSearch: boolean = false;
   private todoItemsSubscription: Subscription | undefined;
   private queryParamsSubscription: Subscription | undefined;
-  
+
   constructor(
-    private todoService: TodoServiceService, 
-    private router: Router, 
-    private route: ActivatedRoute){
-    let url = this.router.url;    
-    if(url === '/bin/clear'){
+    private todoService: TodoServiceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    let url = this.router.url;
+    if (url === '/bin/clear') {
       todoService.fromBin = true;
       todoService.deleteAll();
-    }else if(url === '/home/clear'){
+    } else if (url === '/home/clear') {
       todoService.fromBin = false;
       todoService.deleteAll();
     }
-    this.fromBin = (url.substring(0,5) !== '/home');    
-    this.todoService.fromBin = this.fromBin;    
-   }
+    this.fromBin = url.substring(0, 5) !== '/home';
+    this.todoService.fromBin = this.fromBin;
+  }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.todoService.initializeItems();
-    this.todoItemsSubscription = this.todoService.todoItems$.subscribe(
-      (itemList)=>{
-        this.itemList = itemList.sort((x,y)=>{
-          if(x.setForReminder||y.completionStatus) return -1;
-          if(y.setForReminder||x.completionStatus) return 1;
-          return 0;
-        });
-        },
-      (error)=>{
-        console.error('error fetching tasks ',error);
+    this.queryParamsSubscription = this.route.queryParams.subscribe(
+      (params: Params) => {
+        let filter = params['filter'] ? params['filter'] === 'true' : false;
+        let searchQuery = params['search'] ? params['search'] : '';
+        let tags = params['tag'] ? params['tag'] : [];
+        let searchTerms = params['has'] ? params['has'] : [];
+        if (searchQuery !== '' || tags.length > 0 || searchTerms.length > 0) {
+          this.todoService
+            .searchTodos(searchQuery, tags, searchTerms, filter)
+            .subscribe(
+              (itemList) => {
+                this.fromSearch = true;
+                this.itemList = itemList.sort((x, y) => {
+                  if (x.setForReminder || y.completionStatus) return -1;
+                  if (y.setForReminder || x.completionStatus) return 1;
+                  return 0;
+                });
+              },
+              (error) => {
+                console.error('error fetching tasks ', error);
+              }
+            );
+        } else {
+          this.router.navigate(['/home']); // no re-render here. only url is changed. nothing else happens
+          this.fromSearch=false;
+          this.todoItemsSubscription = this.todoService.todoItems$.subscribe(
+            (itemList) => {
+              this.itemList = itemList.sort((x, y) => {
+                if (x.setForReminder || y.completionStatus) return -1;
+                if (y.setForReminder || x.completionStatus) return 1;
+                return 0;
+              });
+            },
+            (error) => {
+              console.error('error fetching tasks ', error);
+            }
+          );
+        }
       }
     );
-
-      this.queryParamsSubscription = this.route.queryParams.subscribe((params: Params) => {
-      let filter = params['filter']? params['filter'] === 'true': false;
-      let searchQuery = params['search']? params['search']: '';
-      let tags = params['tag']? params['tag']: [];
-      let searchTerms = params['has']?params['has']: [];
-      if(searchQuery !== '' || tags.length > 0 || searchTerms.length > 0)
-      this.todoService.searchTodos(searchQuery, tags, searchTerms, filter).subscribe(
-        (itemList)=>{
-              this.fromSearch = true;
-              this.itemList = itemList.sort((x,y)=>{
-                if(x.setForReminder||y.completionStatus) return -1;
-                if(y.setForReminder||x.completionStatus) return 1;
-                return 0;
-              });     
-            },
-            (error)=>{
-              console.error('error fetching tasks ',error);
-            }
-      );
-    });
   }
   ngOnDestroy(): void {
     if (this.todoItemsSubscription) {
