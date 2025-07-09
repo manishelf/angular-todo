563a7c0a9fceaab50f1c08b2270dc77ae58cabbe
Manish Patil <manishpatilelf@proton.me>
2025-07-09 08:16:14 +0530

    Update home.component.ts

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -52,7 +52,7 @@ export class HomeComponent implements OnInit, OnDestroy {
             });
       }
     }
-    this.fromBin = url.substring(0, 5) !== '/home';
+    this.fromBin = url.substring(0, 5) === '/bin';
     this.todoService.fromBin = this.fromBin;
   }
 
