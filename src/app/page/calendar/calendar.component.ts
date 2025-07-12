c02ea379a71deb6f4d875c60b637525704225a00
Manish patil <manishpatil7058@gmail.com>
2025-07-12 20:07:11 +0530

    better calander experience

--- src/app/page/calendar/calendar.component.ts
+++ src/app/page/calendar/calendar.component.ts
@@ -15,7 +15,7 @@ import { Router } from '@angular/router';
   selector: 'app-calendar',
   standalone: true,
   imports: [CommonModule, FullCalendarModule],
-templateUrl: './calendar.component.html',
+  templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
 export class CalendarComponent {
