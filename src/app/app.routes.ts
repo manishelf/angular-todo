34c78ee5172862e9d9d43ce9a4c4c260987f504f
Manish patil <manishpatil7058@gmail.com>
2025-07-14 21:24:03 +0530

    about page updated for keyboard shortcuts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,18 @@ import { HomeComponent } from './page/home/home.component';
 import { EditorComponent } from './page/editor/editor.component';
 import { CalendarComponent } from './page/calendar/calendar.component';
 import { AboutComponent } from './page/about/about.component';
+import { VisualizeComponent } from './page/visualize/visualize.component';
 
 export const routes: Routes = [
-    {path:'', redirectTo:'home', pathMatch: 'full'},
-    {path:'create', component:EditorComponent},
-    {path:'home', component: HomeComponent},
-    {path: 'bin', component: HomeComponent},
-    {path: 'bin/clear', component: HomeComponent},
-    {path: 'home/clear', component: HomeComponent},
-    {path: 'demo', component: HomeComponent},
-    {path:'edit', component: EditorComponent},
-    {path: 'calendar', component: CalendarComponent},
-    {path: 'about', component: AboutComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'create', component: EditorComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'bin', component: HomeComponent },
+  { path: 'bin/clear', component: HomeComponent },
+  { path: 'home/clear', component: HomeComponent },
+  { path: 'demo', component: HomeComponent },
+  { path: 'edit', component: EditorComponent },
+  { path: 'visualize', component: VisualizeComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'about', component: AboutComponent },
 ];
