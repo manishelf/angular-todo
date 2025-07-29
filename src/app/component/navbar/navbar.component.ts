ea4d5263de20c146ff3ebc3fa505a74f83b30320
Manish Patil <manishpatilelf@proton.me>
2025-07-29 10:19:14 +0530

    Update navbar.component.ts

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -223,6 +223,7 @@ export class NavbarComponent implements AfterViewInit {
       this.todoService.searchTodos(q,tags,has,abs)
         .subscribe((itemList)=>{
           this.sortService.sortItems(ord,itemList,lim).subscribe((itemList)=>{
+            alert('Downloading FILTERED items :'+itemList.length);
             this.save(itemList);
           });
         });
