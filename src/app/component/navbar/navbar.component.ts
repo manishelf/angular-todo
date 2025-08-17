08b4d5cf978203b93969a8f4bd8a842ea16ed042
Manish patil <manishpatil7058@gmail.com>
2025-08-17 13:32:12 +0530

    checkbox options as flat string seperated by ,

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -257,25 +257,10 @@ export class NavbarComponent implements AfterViewInit {
       let reader = new FileReader();
       reader.readAsText(file);
       reader.onload = async (event) => {
-        let json = event.target!.result;
-          if(json){
-            let items = JSON.parse(json.toString());
-            for(let item of items.items){
-              if(item.userDefined?.tag){
-                item.userDefined.tag = {name: item.userDefined.tag};
-              }
-              if(item.userDefined.formControlSchema.fields){
-                for(let field of item.userDefined.formControlSchema.fields){
-                  if(field.options){
-                    console.log(field.options);
-                    
-                    field.options = field.options.join(',');
-                  }
-                }
-              }
-            }
+        let jsonBuff = event.target!.result;
+          if(jsonBuff){
             this.todoService
-            .deserializeManyFromJson(JSON.stringify(items))
+            .deserializeManyFromJson(jsonBuff.toString())
             .subscribe((itemList) => {
               this.todoService.addMany(itemList);
               this.toaster.success('Notes loaded successfully');
